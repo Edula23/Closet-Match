7e05bbbfeff07204af202360e4c19ea84aecb63d
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2026-06-26 10:52:11 +0300

    feat: add dotenv and cors for environment variable management and CORS support

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
 import closetRoutes from "./routes/closetRoutes.js";
 import outfitRoutes from "./routes/outfitRoutes.js";
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -16,7 +21,13 @@ app.use(express.json());
 //Serves the html file from the /public directory
 app.use(express.static(path.join(__dirname, '../public')));
 //tells express to serve all files from the public folder as static assets/files. Any requests for the css  files will be served from the public folder
-
+app.use(cors());
+// app.use(cors({
+//     origin: 'http://localhost:3000', // Replace with your frontend URL
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true, // Allow cookies to be sent
+// }));
 
 
 app.get("/", (req, res) => {
